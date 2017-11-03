06251483f720dd2d36b53d244baeb151aa8fa38d
kimkundad <kim.kundad@gmail.com>
2017-11-03 15:39:29 +0700

    golf

--- public/assets/pages/scripts/login-4.js
+++ public/assets/pages/scripts/login-4.js
@@ -255,10 +255,10 @@ var Login = function () {
 
             // init background slide images
 		    $.backstretch([
-		        "../public/assets/pages/media/bg/1.jpg",
-		        "../public/assets/pages/media/bg/2.jpg",
-		        "../public/assets/pages/media/bg/3.jpg",
-		        "../public/assets/pages/media/bg/4.jpg"
+		        "../assets/pages/media/bg/1.jpg",
+		        "../assets/pages/media/bg/2.jpg",
+		        "../assets/pages/media/bg/3.jpg",
+		        "../assets/pages/media/bg/4.jpg"
 		        ], {
 		          fade: 1000,
 		          duration: 8000
