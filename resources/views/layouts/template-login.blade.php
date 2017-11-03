7d8414f07afd9e538d731c4aa1f0a79482261a9f
kimkundad <kim.kundad@gmail.com>
2017-11-03 07:56:48 +0700

    first commit

---
+++ resources/views/layouts/template-login.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+    <!--<![endif]-->
+    <!-- BEGIN HEAD -->
+
+    <head>
+        <meta charset="utf-8" />
+        <title>Metronic Admin Theme #1 | User Login 6</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta content="Preview page of Metronic Admin Theme #1 for " name="description" />
+        <meta content="" name="author" />
+        <!-- BEGIN GLOBAL MANDATORY STYLES -->
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all"  type="text/css" />
+        <link rel="stylesheet" href="{{url('./assets/global/plugins/font-awesome/css/font-awesome.min.css')}}"  type="text/css" />
+        <link rel="stylesheet" href="{{url('./assets/global/plugins/simple-line-icons/simple-line-icons.min.css')}}"  type="text/css" />
+        <link rel="stylesheet" href="{{url('./assets/global/plugins/bootstrap/css/bootstrap.min.css')}}"  type="text/css" />
+        <link rel="stylesheet" href="{{url('./assets/global/css/custom.min.css')}}"  type="text/css" />
+        <!-- END GLOBAL MANDATORY STYLES -->
+        <!-- BEGIN PAGE LEVEL PLUGINS -->
+
+        <!-- END PAGE LEVEL PLUGINS -->
+        <!-- BEGIN THEME GLOBAL STYLES -->
+        <link rel="stylesheet" href="{{url('./assets/global/css/components.min.css')}}" id="style_components" type="text/css" />
+        <link rel="stylesheet" href="{{url('./assets/global/css/plugins.min.css')}}"  type="text/css" />
+        <!-- END THEME GLOBAL STYLES -->
+        <!-- BEGIN PAGE LEVEL STYLES -->
+        <link rel="stylesheet" href="{{url('./assets/pages/css/login-2.min.css')}}"  type="text/css" />
+        <!-- END PAGE LEVEL STYLES -->
+        <!-- BEGIN THEME LAYOUT STYLES -->
+        <!-- END THEME LAYOUT STYLES -->
+        <link rel="shortcut icon" href="favicon.ico" /> </head>
+    <!-- END HEAD -->
+
+    <body class=" login">
+        <!-- BEGIN : LOGIN PAGE 5-2 -->
+        <div class="logo">
+          <a href="#">
+              <img src="{{asset('./assets/pages/img/logo-big-white.png')}}" style="height: 17px;" alt=""> </a>
+        </div>
+
+                    @yield('content')
+
+
+        <!-- END : LOGIN PAGE 5-2 -->
+        <!--[if lt IE 9]>
+<script src="../assets/global/plugins/respond.min.js"></script>
+<script src="../assets/global/plugins/excanvas.min.js"></script>
+<script src="../assets/global/plugins/ie8.fix.min.js"></script>
+<![endif]-->
+        <!-- BEGIN CORE PLUGINS -->
+        <script src="{{asset('./assets/global/plugins/jquery.min.js')}}" type="text/javascript"></script>
+        <script src="{{asset('./assets/global/plugins/bootstrap/js/bootstrap.min.js')}}" type="text/javascript"></script>
+        <script src="{{asset('./assets/global/plugins/js.cookie.min.js')}}" type="text/javascript"></script>
+        <script src="{{asset('./assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js')}}" type="text/javascript"></script>
+        <script src="{{asset('./assets/global/plugins/jquery.blockui.min.js')}}" type="text/javascript"></script>
+        <!-- END CORE PLUGINS -->
+        <!-- BEGIN PAGE LEVEL PLUGINS -->
+        <script src="{{asset('./assets/global/plugins/jquery-validation/js/jquery.validate.min.js')}}" type="text/javascript"></script>
+        <script src="{{asset('./assets/global/plugins/jquery-validation/js/additional-methods.min.js')}}" type="text/javascript"></script>
+        <script src="{{asset('./assets/global/plugins/backstretch/jquery.backstretch.js')}}" type="text/javascript"></script>
+        <!-- END PAGE LEVEL PLUGINS -->
+        <!-- BEGIN THEME GLOBAL SCRIPTS -->
+        <script src="{{asset('./assets/global/scripts/app.js')}}" type="text/javascript"></script>
+        <!-- END THEME GLOBAL SCRIPTS -->
+        <!-- BEGIN PAGE LEVEL SCRIPTS -->
+        <script src="{{asset('./assets/pages/scripts/login.min.js')}}" type="text/javascript"></script>
+        <!-- END PAGE LEVEL SCRIPTS -->
+        <!-- BEGIN THEME LAYOUT SCRIPTS -->
+        <!-- END THEME LAYOUT SCRIPTS -->
+    </body>
+
+</html>
